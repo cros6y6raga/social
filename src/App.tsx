790d09f9e2ff2b1177c99d5f52c6d77af12ac3ba
cros6y6raga <crosbybraga@yandex.ru>
2022-09-15 21:12:21 +0300

    lesson 07: What is a Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+    return (
+        <div>
+            <Header/>
+          <Technologies/>
+        </div>
+    )
+}
+const Technologies = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+      <div>
+          <ul>
+              <li>css</li>
+              <li>html</li>
+              <li>js</li>
+              <li>react</li>
+          </ul>
+      </div>
+  )
+}
+const Header = () => {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>New Feed</a>
+            <a href='#'>Messages</a>
+        </div>
+    )
 }
 
 export default App;
