bf90003743f9e99a6a949bc00eced32a9074ccc7
cros6y6raga <crosbybraga@yandex.ru>
2022-09-17 16:10:10 +0300

    lesson 14: css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png" alt="logo"/>
         </header>
     );
