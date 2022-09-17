3736c8d210cc5fe1b70f13b5753d898712e8e8a0
cros6y6raga <crosbybraga@yandex.ru>
2022-09-17 13:46:42 +0300

    lesson 12: Breakdown into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png" alt="logo"/>
+        </header>
+    );
+};
